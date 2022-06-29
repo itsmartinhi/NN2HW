921472cda6f56c82051bc6ac2b20ea04213a6f5c
Max <max.schunck@gmail.com>
2022-06-29 13:27:08 +0200

    improve neuron decrementer

--- Hardware/NeuronDec/neurondec.vhdl
+++ Hardware/NeuronDec/neurondec.vhdl
@@ -7,7 +7,7 @@ entity NEURONDEC is
 		   reset: 			 in  std_logic;
 		   clk: 			 in  std_logic;
 		   c_neuron_dec:  	 in  std_logic;
-		   out_neuron_indx:  out std_logic_vector(3 downto 0);
+		   out_neuron_index: out std_logic_vector(3 downto 0);
 		   out_neuron_reset: out std_logic
 	);
 end NEURONDEC;
@@ -19,15 +19,17 @@ begin
 	begin
 		if rising_edge(clk) then
 			if reset = '1' then
-				count <= "01001";
+				count <= "01000";
+				out_neuron_index <= "1001";
 			elsif c_neuron_dec = '1' then
 				if count(4) = '1' then 
-					count <= "01001";
+					count <= "01000";
 					out_neuron_reset <= '1';
+					out_neuron_index <= "1001";
 				else 
-					count <= std_logic_vector(signed(count) - 1);
-					out_neuron_indx <= count(3 downto 0);
+					out_neuron_index <= count(3 downto 0);
 					out_neuron_reset <= '0';
+					count <= std_logic_vector(signed(count) - 1);
 				end if;
 			end if;
 		end if;
--- Hardware/NeuronDec/neurondec_tb.vhdl
+++ Hardware/NeuronDec/neurondec_tb.vhdl
@@ -12,7 +12,7 @@ component NEURONDEC is
 		reset: 				in 	std_logic;
 		clk: 			 	in  std_logic;
 		c_neuron_dec:  		in  std_logic;
-		out_neuron_indx:  	out std_logic_vector(3 downto 0);
+		out_neuron_index:  	out std_logic_vector(3 downto 0);
 		out_neuron_reset: 	out std_logic
 	);
 end component NEURONDEC;
@@ -22,7 +22,7 @@ for SPEC: NEURONDEC use entity WORK.NEURONDEC(RTL);
 signal reset: 			 	std_logic;
 signal clk: 			  	std_logic;
 signal c_neuron_dec:  	  	std_logic;
-signal spec_neuron_indx:  	std_logic_vector(3 downto 0);
+signal spec_neuron_index:  	std_logic_vector(3 downto 0);
 signal spec_neuron_reset: 	std_logic;
 signal tb_count: 		  	std_logic_vector(4 downto 0);
 
@@ -31,7 +31,7 @@ begin
 		reset				=> reset,
 		clk 			 	=> clk,
 		c_neuron_dec 	 	=> c_neuron_dec,
-		out_neuron_indx  	=> spec_neuron_indx,
+		out_neuron_index  	=> spec_neuron_index,
 		out_neuron_reset 	=> spec_neuron_reset
 	);
 
@@ -51,18 +51,18 @@ begin
 		reset <= '0';
 	
 		c_neuron_dec <= '1';
-		tb_count <= "01001";
-		for i in 0 to 9 loop 
+		tb_count <= "01000";
+		for i in 0 to 8 loop 
 			run_cycle;
 			tb_count <= std_logic_vector(signed(tb_count) - 1);
-			assert spec_neuron_indx = tb_count(3 downto 0) report "tb failed on increment:" & integer'image(i);
+			assert spec_neuron_index = tb_count(3 downto 0) report "tb failed on increment:" & integer'image(i);
 			assert spec_neuron_reset = '0' report "reset signal is not 0 while looping on increment:" & integer'image(i);
 		end loop;
 		run_cycle;
 		tb_count <= std_logic_vector(signed(tb_count) - 1);
 		assert spec_neuron_reset = '1' report "reset not set";
 		run_cycle;
-		assert spec_neuron_indx = "1001" report "count didnt got reseted";
+		assert spec_neuron_index = "1000" report "count didnt got reseted";
 		
 		
 		report "tb finished OK";
