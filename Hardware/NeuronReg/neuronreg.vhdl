ea19a953fc0ff1941b482e6512d11dc9121b5031
Max <max.schunck@gmail.com>
2022-06-29 19:15:42 +0200

    updated Neuron Reg

--- Hardware/NeuronReg/neuronreg.vhdl
+++ Hardware/NeuronReg/neuronreg.vhdl
@@ -3,13 +3,13 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity NEURONREG is
-port (
-	clk: 			in 	std_logic;
-	reset: 			in 	std_logic;
-	c_nreg: 		in 	std_logic;						
-	in_nreg_val: 	in 	std_logic_vector(11 downto 0);
-	out_nreg_val: 	out std_logic_vector(20 downto 0)
-);
+	port (
+		clk: 			in 	std_logic;
+		reset: 			in 	std_logic;
+		c_nreg: 		in 	std_logic;						
+		in_nreg_val: 	in 	std_logic_vector(11 downto 0);
+		out_nreg_val: 	out std_logic_vector(20 downto 0)
+	);
 end NEURONREG;
 
 architecture RTL of NEURONREG is
