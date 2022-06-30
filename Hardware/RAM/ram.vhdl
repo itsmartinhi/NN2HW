49e34c18222379a015a6b8d823ba4a050c9f8c31
Martin Hintersberger <martin.hintersberger@gmail.com>
2022-06-30 10:36:31 +0200

    update ram & rom

--- Hardware/RAM/ram.vhdl
+++ Hardware/RAM/ram.vhdl
@@ -4,9 +4,8 @@ use ieee.numeric_std.all;
 
 entity RAM is
     port (
-        reset:              in std_logic;
         in_ram_input_index: in std_logic_vector(7 downto 0); -- this is the given memory address
-        out_inputs:         out std_logic_vector(11 downto 0) -- this is the output datastream
+        out_data_ram:       out std_logic_vector(11 downto 0) -- this is the output datastream
     );
 end RAM;
 
@@ -215,5 +214,5 @@ architecture RTL of RAM is
     );
 
 begin
-    out_inputs <= ram_arr(to_integer(unsigned(in_ram_input_index)));
+    out_data_ram <= ram_arr(to_integer(unsigned(in_ram_input_index)));
 end RTL;
--- Hardware/ROM/rom_tb.vhdl
+++ Hardware/ROM/rom_tb.vhdl
@@ -11,7 +11,7 @@ component ROM is
     port (
         in_rom_input_index:     in std_logic_vector(7 downto 0); -- the given rom input index
         in_rom_neuron_index:    in std_logic_vector(3 downto 0); -- the given neuron index
-        out_weights:            out std_logic_vector(31 downto 0) -- the output datastream
+        out_data_rom:               out std_logic_vector(31 downto 0) -- the output datastream
     );
 end component ROM;
 
@@ -19,23 +19,24 @@ for SPEC: ROM use entity WORK.ROM(RTL);
 
 signal in_rom_input_index:  std_logic_vector(7 downto 0);
 signal in_rom_neuron_index: std_logic_vector(3 downto 0);
-signal out_weights:         std_logic_vector(31 downto 0);
+signal out_data_rom:        std_logic_vector(31 downto 0);
 
 begin
     SPEC: ROM port map (
         in_rom_input_index  => in_rom_input_index,
         in_rom_neuron_index => in_rom_neuron_index,
-        out_weights         => out_weights
+        out_data_rom        => out_data_rom
     );
 
     process
     begin
-        wait for 1 ns; -- this is needed for some reason
 
         in_rom_input_index <= "00000000";
         in_rom_neuron_index <= "0000";
 
-        assert out_weights = "01100011000000000000000000000000" report "output not as expected!";
+        wait for 1 ns; -- this is needed for some reason
+
+        assert out_data_rom = "01100011000000000000000000000000" report "output not as expected!";
 
         report "rom tb finished.";
         wait;
