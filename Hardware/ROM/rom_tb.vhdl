a291c94cbeff2050c3cb3905d50d244130fad059
Martin Hintersberger <martin.hintersberger@gmail.com>
2022-06-30 08:41:19 +0200

    add clk and reset to rom

---
+++ Hardware/ROM/rom_tb.vhdl
@@ -0,0 +1,14 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity ROM_TB is
+end ROM_TB;
+
+architecture TESTBENCH of ROM_TB is
+
+component ROM_TB is
+    port (
+
+    );
+end component ROM_TB;
