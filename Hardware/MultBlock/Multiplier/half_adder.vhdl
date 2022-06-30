9d96998de6ce37a58324cbe60bcffe735bab5bc3
mosermat <matthias.moser@hs-augsburg.de>
2022-06-30 08:58:58 +0200

    syn

--- Hardware/MultBlock/Multiplier/half_adder.vhdl
+++ Hardware/MultBlock/Multiplier/half_adder.vhdl
@@ -1,54 +1,52 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
 
+ENTITY HALF_ADDER IS
+  PORT (
+    a, b : IN STD_LOGIC;
+    sum, carry : OUT STD_LOGIC);
+END HALF_ADDER;
 
+-- ARCHITECTURE BEHAVIOR OF HALF_ADDER IS
+-- BEGIN
 
-entity HALF_ADDER is
-  port (a, b: in std_logic; sum, carry: out std_logic);
-end HALF_ADDER;
+--   PROCESS (a, b)
+--     VARIABLE a2, b2, result : unsigned (1 DOWNTO 0);
+--   BEGIN
+--     a2 := '0' & a; -- extend 'a' to 2 bit
+--     b2 := '0' & b; -- extend 'b' to 2 bit
+--     result := a2 + b2; -- add them
+--     sum <= result(0); -- output 'sum' = lower bit
+--     carry <= result(1); -- output 'carry' = upper bit
+--   END PROCESS;
 
+-- END BEHAVIOR;
 
+ARCHITECTURE DATAFLOW OF HALF_ADDER IS
+BEGIN
+  sum <= a XOR b;
+  carry <= a AND b;
+END DATAFLOW;
 
-architecture BEHAVIOR of HALF_ADDER is
-begin
+ARCHITECTURE STRUCTURE OF HALF_ADDER IS
 
-  process (a, b)
-    variable a2, b2, result: unsigned (1 downto 0);
-  begin
-    a2 := '0' & a;      -- extend 'a' to 2 bit
-    b2 := '0' & b;      -- extend 'b' to 2 bit
-    result := a2 + b2;  -- add them
-    sum <= result(0);   -- output 'sum' = lower bit
-    carry <= result(1); -- output 'carry' = upper bit
-  end process;
+  COMPONENT XOR2
+    PORT (
+      x, y : IN STD_LOGIC;
+      z : OUT STD_LOGIC);
+  END COMPONENT;
 
-end BEHAVIOR;
+  COMPONENT AND2
+    PORT (
+      x, y : IN STD_LOGIC;
+      z : OUT STD_LOGIC);
+  END COMPONENT;
 
+  -- for I0: XOR2 use entity WORK.XOR2(DATAFLOW);
+  -- for I1: AND2 use entity WORK.AND2(DATAFLOW);
 
-
-architecture DATAFLOW of HALF_ADDER is
-begin
-  sum <= a xor b;
-  carry <= a and b;
-end DATAFLOW;
-
-
-
-architecture STRUCTURE of HALF_ADDER is
-
-  component XOR2
-    port (x, y: in std_logic; z: out std_logic);
-  end component;
-
-  component AND2
-    port (x, y: in std_logic; z: out std_logic);
-  end component;
-
-  for I0: XOR2 use entity WORK.XOR2(DATAFLOW);
-  for I1: AND2 use entity WORK.AND2(DATAFLOW);
-
-begin
-  I0: XOR2 port map(x => a, y => b, z => sum);
-  I1: AND2 port map(x => a, y => b, z => carry);
-end STRUCTURE;
+BEGIN
+  I0 : XOR2 PORT MAP(x => a, y => b, z => sum);
+  I1 : AND2 PORT MAP(x => a, y => b, z => carry);
+END STRUCTURE;
