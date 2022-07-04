1f312d00fbcf701814829d7d161c675c313e5df5
mosermat <matthias.moser@hs-augsburg.de>
2022-07-04 16:53:59 +0200

    added 11 bit and 12 bit adder and rewrote makefile

--- Hardware/MultBlock/Multiplier/analyse.bash
+++ Hardware/MultBlock/Multiplier/analyse.bash
@@ -1,3 +1,5 @@
-ees-ghdl -a and2.vhdl or2.vhdl xor2.vhdl half_adder.vhdl full_adder.vhdl multiplier.vhdl multiplier_tb.vhdl
+ees-ghdl -a and2.vhdl or2.vhdl xor2.vhdl half_adder.vhdl full_adder.vhdl multiplier.vhdl multiplier_tb.vhdl ElevenBitAdder.vdhl TwelveBitAdder.vdhl multblock.vhdl multblock_tb.vhdl
 ees-ghdl -a multiplier_final.vhdl multiplier_tb.vhdl
 ees-ghdl -r multiplier_tb --wave=multiplier_tb_wave.ghw
+
+ees-ghdl -a elevenBitAdder.vdhl
